e2087d4aa3d54d87d315f8cc859c485bfb37b70c
Janos Molnar <janosmolnar1001@gmail.com>
2019-10-09 21:23:19 +0200

    Init project

---
+++ README.rst
@@ -0,0 +1,2 @@
+# zuora-aqua-client-cli
+Run ZOQL queries through AQuA from the command line
---
+++ tests/test_zuora_aqua_client_cli.py
@@ -0,0 +1,5 @@
+from zuora_aqua_client_cli import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
